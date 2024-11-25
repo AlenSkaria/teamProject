15db8c4ea5e4e1aaa94617784589931dd49ca9a8
AlenSkaria <hello.skaria@gmail.com>
2024-11-25 17:13:55 +0530

    do work

--- models.py/employee.py
+++ models.py/employee.py
@@ -1,6 +1,17 @@
 class Employee:
-    def __init__(self,employeeName,age):
-        self.employeeName = employeeName
+    def __init__(self,name,age,doj,createdOn,isActive):
+        self.name = name
         self.age = age
+        self.doj = doj
+        self.__createdOn=createdOn
+        self.isActive=isActive
+
+
+    def getCO(self):
+        return self.__createdOn
+
+    def setCO(self,createdOn):
+        self.__createdOn = createdOn
+
     def display(self):
-        print(f'the emp name is {self.employeeName}')
+        print(f'the emp name is {self.employeeName}, {self.age},{self.doj},{self.getCO()},')
